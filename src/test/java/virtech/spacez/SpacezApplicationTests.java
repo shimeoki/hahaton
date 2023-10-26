753ca84bb3b935103defd5cf2f3e9505a438085d
dimazzzik <79855381+dimazzz1k@users.noreply.github.com>
2023-10-26 21:05:02 +0300

    Add Spring

---
+++ src/test/java/virtech/spacez/SpacezApplicationTests.java
@@ -0,0 +1,13 @@
+package virtech.spacez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
