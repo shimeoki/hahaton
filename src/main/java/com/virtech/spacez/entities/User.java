e47c54f7856d485c452cd3053f9266d2f1d75516
shimeoki <dimazzzik1337@gmail.com>
2023-10-27 12:34:27 +0300

    feat. Add User ORM

--- src/main/java/com/virtech/spacez/entities/User.java
+++ src/main/java/com/virtech/spacez/entities/User.java
@@ -1,25 +1,38 @@
 package com.virtech.spacez.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "surname")
     private String surname;
 
+    @Column(name = "login")
     private String login;
 
-    private String password_bcrypt;
+    @Column(name = "password_bcrypt")
+    private String passwordBcrypt;
     
-    private Integer role_id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
 
     public Integer getId() {
         return id;
@@ -43,5 +56,29 @@ public class User {
 
     public void setSurname(String surname) {
         this.surname = surname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPasswordBcrypt() {
+        return passwordBcrypt;
+    }
+
+    public void setPasswordBcrypt(String passwordBcrypt) {
+        this.passwordBcrypt = passwordBcrypt;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 }
--- src/main/java/com/virtech/spacez/repositories/RoleRepository.java
+++ src/main/java/com/virtech/spacez/repositories/RoleRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.virtech.spacez.entities.Role;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called roleRepository
-// CRUD refers Create, Read, Update, Delete
 
 public interface RoleRepository extends CrudRepository<Role, Integer> {
     
