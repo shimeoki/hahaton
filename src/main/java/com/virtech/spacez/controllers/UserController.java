e57357c7b2b0025d7de0e987023ab67bcdc79356
shimeoki <dimazzzik1337@gmail.com>
2023-10-27 15:03:54 +0300

    feat. ORM changes 1. /api for controllers 2. SatelliteOrbit 3. UserController (WIP) 4. id int -> long 5. Change variable names to Java code convention 6. Decorator changes 7. User (WIP)

---
+++ src/main/java/com/virtech/spacez/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.virtech.spacez.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.virtech.spacez.entities.User;
+import com.virtech.spacez.repositories.UserRepository;
+
+@Controller
+@RequestMapping(path = "/api")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+}
