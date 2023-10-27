db7019b471cb4d1b7c6d8b7962ce4878041d1b95
shimeoki <dimazzzik1337@gmail.com>
2023-10-27 10:25:35 +0300

    feat. add User.java (WIP)

---
+++ src/main/java/com/virtech/spacez/entities/User.java
@@ -0,0 +1,47 @@
+package com.virtech.spacez.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String surname;
+
+    private String login;
+
+    private String password_bcrypt;
+    
+    private Integer role_id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
