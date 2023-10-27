d947ad6ab82c9b85d137f597bcfec2562c35d125
Traunin <traunin5@gmail.com>
2023-10-27 12:51:24 +0300

    finished login/signup

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+var login_selected = true;
 
 $(".profile").on("click", function(){
     if($(".login-signup").hasClass("shown")) {
@@ -11,5 +12,16 @@ $(".selection span").on("click", function(){
     if(!$(this).hasClass("selected")) {
         $(".selection span").removeClass("selected")
         $(this).addClass("selected")
+
+        $(".login-signup form").removeClass("hidden")
+        if(login_selected) {
+            $(".login-signup form:nth-child(2)").addClass("hidden")
+        } else {
+            $(".login-signup form:nth-child(3)").addClass("hidden")
+        }
+        login_selected = !login_selected
+        console.log(login_selected)
     }
+
+    
 })
