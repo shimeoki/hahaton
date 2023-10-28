4d1a674a735a1153358746477e29c0fa85526ea6
Traunin <traunin5@gmail.com>
2023-10-28 12:19:48 +0300

    Working orbits

--- scripts/visualisation.js
+++ scripts/visualisation.js
@@ -3,6 +3,47 @@
         import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js'
 
 
+        var orbits = []
+        $(document).on('change keyup paste click', 'body', function()
+        {   
+            var orbitParams = []
+            $('.satelite').each(function(){
+                var inputs = $(this).children().children()
+                var name = inputs[1].value
+                var angle1 = inputs[3].value
+                var angle2 = inputs[5].value
+                var a = inputs[7].value
+                var b = inputs[9].value
+                if(name != "" && angle1 != "" && angle2 != "" && a != "" && b != ""){
+                    orbitParams.push({'name': name, 'angle1': angle1, 'angle2':angle2, 'a': a, 'b':b})
+                }
+            })
+            
+            orbits.forEach((orbit)=>scene.remove(orbit))
+            orbitParams.forEach((orbit)=>{
+                //draw ellipse
+                const ovalShape = new THREE.Shape();
+                ovalShape.ellipse(0, 0, orbit.a, orbit.b, 0, 2 * Math.PI);
+
+                // Create a path geometry for the contour
+                const geometry = new THREE.BufferGeometry().setFromPoints(ovalShape.getPoints());
+
+                // Create a material for the contour (e.g., a basic line material)
+                const material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+
+                // Create a line using the geometry and material
+                const contour = new THREE.Line(geometry, material);
+                contour.rotateX(orbit.angle1*Math.PI)
+                contour.rotateY(orbit.angle2*Math.PI)
+
+                // Add the contour to the scene
+                scene.add(contour);
+                orbits.push(contour)
+            })
+
+            console.log(orbitParams)
+        });
+
         function getCenterPoint(mesh) {
             var middle = new THREE.Vector3();
             var geometry = mesh.geometry;
@@ -58,26 +99,6 @@
             earth.position.setY(coords.y)
             earth.position.setZ(coords.z)
         })
-
-
-
-        //draw ellipse
-        const ovalShape = new THREE.Shape();
-        ovalShape.ellipse(0, 0, 2, 1, 0, 2 * Math.PI);
-
-        // Create a path geometry for the contour
-        const geometry = new THREE.BufferGeometry().setFromPoints(ovalShape.getPoints());
-
-        // Create a material for the contour (e.g., a basic line material)
-        const material = new THREE.LineBasicMaterial({ color: 0x00ff00 });
-
-        // Create a line using the geometry and material
-        const contour = new THREE.Line(geometry, material);
-
-        // Add the contour to the scene
-        scene.add(contour);
-
-        
 
         const tick = () => {
             controls.update();
